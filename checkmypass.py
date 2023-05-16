a68b8a559fe2609fef9c72007f6920459e176136
Tarun Mittal <tm6622@g.rit.edu>
2023-05-16 01:25:19 -0400

    added main for custom args

--- checkmypass.py
+++ checkmypass.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 def request_api_data(query_char):
     url = 'https://api.pwnedpasswords.com/range/' + query_char
@@ -11,7 +12,9 @@ def request_api_data(query_char):
 def get_password_leaks_count(hashes, hash_to_check):
     hashes = (line.split(':') for line in hashes.text.splitlines())
     for h, count in hashes:
-        print(h, count)
+        if h == hash_to_check:
+            return count
+    return 0
         
 
 def pwned_api_check(password):
@@ -22,4 +25,13 @@ def pwned_api_check(password):
     return get_password_leaks_count(response, tail)
 
 
-pwned_api_check('123')
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times... you should probably change your password!')
+        else:
+            print(f'{password} was NOT found. Carry on!')
+    return 'done!'
+
+main(sys.argv[1:])
